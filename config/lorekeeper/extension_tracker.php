0ffce84a92b4f951362897369cfc0fb68c4de1c6
Draginraptor <draginraptor@gmail.com>
2021-05-19 23:11:54 +0800

    - Added information to extension tracker

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -12,5 +12,12 @@ return [
     //     'version' => '1.0.0',
     // ],
     
-
+    'is_online' => [
+        'key' => 'is_online',
+        'wiki_key' => 'Is_Online',
+        'creators' => json_encode([
+            'Uri' => 'https://github.com/preimpression/',
+        ]),
+        'version' => '1.0.0',
+    ],
 ];
